c7d0f8fa49ca1227abbcfc90a26393c41908f748
Lam Thai Thanh Long <lamthaithanhlong@gmail.com>
2024-04-03 14:13:34 -0500

    update

--- lab1b/employeepensionscliapp/src/main/java/edu/miu/cs489/lab1b/EmployeePensionsMgmtApp.java
+++ lab1b/employeepensionscliapp/src/main/java/edu/miu/cs489/lab1b/EmployeePensionsMgmtApp.java
@@ -21,14 +21,12 @@ public class EmployeePensionsMgmtApp {
         employees.get(0).setPensionPlan(new PensionPlan("EX1089", LocalDate.of(2023, 1, 17), new BigDecimal("100.00")));
 
         Employee benard = new Employee(2, "Benard", "Shaw", LocalDate.of(2019, 4, 3), new BigDecimal("197750.00"));
-        // Assuming Benard does not have a pension plan yet.
         employees.add(benard);
 
         employees.add(new Employee(3, "Carly", "Agar", LocalDate.of(2014, 5, 16), new BigDecimal("842000.75")));
         employees.get(2).setPensionPlan(new PensionPlan("SM2307", LocalDate.of(2019, 11, 4), new BigDecimal("1555.50")));
 
         Employee wesley = new Employee(4, "Wesley", "Schneider", LocalDate.of(2019, 5, 2), new BigDecimal("74500.00"));
-        // Assuming Wesley does not have a pension plan yet.
         employees.add(wesley);
 
         ObjectMapper mapper = new ObjectMapper();
