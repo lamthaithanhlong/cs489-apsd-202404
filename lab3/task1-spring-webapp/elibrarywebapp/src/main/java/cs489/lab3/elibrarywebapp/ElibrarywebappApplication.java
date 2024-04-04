10a2be0299bec6c1cd651785211721ae8fa0b5da
Prof OKalu <okalu1@outlook.com>
2024-04-04 16:11:37 -0500

    Add solutions for Lab2 and Lab3 task1 starter

---
+++ lab3/task1-spring-webapp/elibrarywebapp/src/main/java/cs489/lab3/elibrarywebapp/ElibrarywebappApplication.java
@@ -0,0 +1,13 @@
+package cs489.lab3.elibrarywebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElibrarywebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElibrarywebappApplication.class, args);
+	}
+
+}
---
+++ lab3/task1-spring-webapp/elibrarywebapp/src/test/java/cs489/lab3/elibrarywebapp/ElibrarywebappApplicationTests.java
@@ -0,0 +1,13 @@
+package cs489.lab3.elibrarywebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElibrarywebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
